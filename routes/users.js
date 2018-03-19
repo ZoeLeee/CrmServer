89d3093a7cb633833eb9022a8dcfbb80ed175ee2
Zoe <lizhou1021@qq.com>
2018-03-19 23:25:33 +0800

    add register

--- routes/users.js
+++ routes/users.js
@@ -24,4 +24,22 @@ router.post('/login', (req, res, next) => {
     }
   })
 })
+router.get('/register', (req, res, next) => {
+  var info=req.query;
+  console.log('info: ', info);
+  // handler(req, res, "user", { uname: req.body.uname }, data => {
+  //   console.log(data);
+  //   // if (data.length === 0) {
+  //   //   res.end('{"err":"抱歉，系统中并无该用户，如有需要，请向管理员申请"}');
+  //   // } else if (data[0].pwd !== req.body.upwd) {
+  //   //   res.end('{"err":"密码不正确"}');
+  //   // } else if (data.length !== 0 && data[0].pwd === req.body.upwd) {
+
+  //   //   req.session.username = req.body.uname; //存session
+  //   //   req.session.password = password;
+
+  //   //   res.end('{"success":true}');
+  //   // }
+  // })
+})
 module.exports = router;
